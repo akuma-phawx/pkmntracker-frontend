4f185c54cc83e1b532d6399753b18c0fda854c91
Christoforos Vradis <christoforos.vradis@onecentral.nl>
2024-05-20 18:59:49 +0200

    wip: fetching sets from backend

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -1,12 +1,11 @@
 <template>
-    <v-footer app>
-      <v-col class="text-center">© 2024 My App</v-col>
-    </v-footer>
-  </template>
-  
-  <script setup>
-  </script>
-  
-  <style scoped>
-  /* Add Footer-specific styles here */
-  </style>
+  <v-footer app>
+    <v-col class="text-center">© 2024 Pkmntracker Team</v-col>
+  </v-footer>
+</template>
+
+<script setup></script>
+
+<style scoped>
+/* Add Footer-specific styles here */
+</style>
--- src/pages/sets/SetsOverview.vue
+++ src/pages/sets/SetsOverview.vue
@@ -1,4 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import type { Set } from "@/types/Set";
+
+const sets = ref<Set[]>();
+
+onMounted(async () => {
+  const response = await fetch("http://localhost:8080/api/sets");
+});
+</script>
 <template>
   <h3>Sets Page</h3>
+  {{ sets }}
 </template>
